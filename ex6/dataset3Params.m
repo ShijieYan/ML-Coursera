cbff988d68ef6dccc51eaf532444ab18731881da
ShijieYan <yanshijie1991@gmail.com>
2021-05-09 22:42:43 -0400

    Assignment #6 completed
    100/100

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -22,13 +22,22 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
-
-
-
-
-
-
-
+vals = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+n = length(vals);
+errors = zeros(n, n);
+for i = 1:n
+    for j = 1:n
+        C = vals(i);
+        sigma = vals(j);
+        model = svmTrain(X, y, C, @(x1, x2)gaussianKernel(x1, x2, sigma));
+        predictions = svmPredict(model, Xval);
+        errors(i,j) = mean(double(predictions ~= yval));
+    end
+end
+[~, I] = min(errors(:));
+[row, col] = ind2sub([n, n], I);
+C = vals(row);
+sigma = vals(col);
 % =========================================================================
 
 end
