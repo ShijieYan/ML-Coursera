5275b8a50bf27eb9d5f61644e63aaeb3bdfc8da0
ShijieYan <yanshijie1991@gmail.com>
2021-05-09 22:42:00 -0400

    Assignment #5 completed
    100/100

--- ex5/linearRegCostFunction.m
+++ ex5/linearRegCostFunction.m
@@ -18,18 +18,12 @@ grad = zeros(size(theta));
 %
 %               You should set J to the cost and grad to the gradient.
 %
+h = X * theta;
+J = 1/(2*m)*(sum((h - y).^2,1) + lambda*(sum(theta(2:end).^2,1)));
 
-
-
-
-
-
-
-
-
-
-
-
+% gradient
+grad = 1/m*X'*(h-y);
+grad(2:end) = grad(2:end) + lambda/m*theta(2:end);
 % =========================================================================
 
 grad = grad(:);
